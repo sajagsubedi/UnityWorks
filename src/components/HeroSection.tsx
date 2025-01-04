54b5e0f14e4a6d68a0d15e21afa9ce54accbb65f
Sajag Subedi <sajagsubedi03@gmail.com>
2025-01-04 12:20:18 +0545

    Hero Section Carousel improved

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,69 +1,69 @@
 "use client";
-import React, { useEffect, useState } from "react";
+
+import React from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import { FaChevronRight, FaChevronLeft} from "react-icons/fa";
+import Autoplay from "embla-carousel-autoplay";
 import banner1 from "@/assets/banner-1.jpg";
 import banner2 from "@/assets/banner-2.jpg";
 import banner3 from "@/assets/banner-3.jpg";
 import Image from "next/image";
-import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
 
-export default function Hero() {
-  const [banner, setBanner] = useState(0);
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setBanner((p) => {
-        const addV = p + 1;
-        return addV % 3;
-      });
-    }, 7000);
-    return () => clearInterval(intervalId);
-  }, []);
-  
+export default function Testimonials() {
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [
+    Autoplay({ delay: 5000 }),
+  ]);
+
+  const scrollPrev = React.useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev();
+  }, [emblaApi]);
+
+  const scrollNext = React.useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext();
+  }, [emblaApi]);
+
   return (
-    <section className="w-full max-h-[calc(100vh-80px)]  relative overflow-hidden">
-      <div
-        className={`flex transform -translate-x-[${
-          banner * 100
-        }%] transition-all`}
-      >
-        <Image
-          className="min-w-full max-h-[calc(100vh-80px)] object-fill"
-          alt="banner"
-          src={banner1}
-        />
+    <div className="w-full">
+      <div className="relative w-full mx-auto flex">
+        <div className="overflow-hidden relative -z-10 flex" ref={emblaRef}>
+          <div className="flex">
+            <div className="relative min-w-full">
+              <Image
+                className="w-screen max-h-[70vh]"
+                alt="banner"
+                src={banner1}
+              />
+            </div>
+            <div className="relative min-w-full">
+              <Image
+                className="w-screen max-h-[70vh]"
+                alt="banner"
+                src={banner2}
+              />
+            </div>
+            <div className="relative min-w-full">
+              <Image
+                className="w-screen max-h-[70vh]"
+                alt="banner"
+                src={banner3}
+              />
+            </div>
+          </div>
+        </div>
 
-        <Image
-          className="min-w-full max-h-[calc(100vh-80px)] object-fill"
-          alt="banner"
-          src={banner2}
-        />
-        <Image
-          className="min-w-full max-h-[calc(100vh-80px)] object-fill"
-          alt="banner"
-          src={banner3}
-        />
-      </div>
-      <div className="absolute flex justify-between w-full top-[50%] px-2">
         <button
-          className="px-1 py-1  rounded-full hover:bg-white"
-          onClick={() => {
-            setBanner((p) => {
-              return (p - 1) % 3;
-            });
-          }}
+          onClick={scrollPrev}
+          className="rounded-full p-2 absolute left-5 top-1/2 transform -translate-y-1/2 -translate-x-1/2"
         >
-          <FaAngleLeft className="text-green-500" />
+          <FaChevronLeft className="w-6 h-6 text-green-500" />
         </button>
         <button
-          className="px-1 py-1  rounded-full hover:bg-white"
-          onClick={() => {
-            setBanner((p) => {
-              return (p + 1) % 3;
-            });
-          }}
+          onClick={scrollNext}
+          className="rounded-full p-2 absolute right-5 top-1/2 transform -translate-y-1/2 translate-x-1/2"
         >
-          <FaAngleRight className="text-green-500" />
+          <FaChevronRight className="w-6 h-6 text-green-500" />
         </button>
       </div>
-    </section>
+    </div>
   );
 }

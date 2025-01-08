cf8711689f7fdf0d93bc61b0bb40a91c53524e98
Sajag Subedi <sajagsubedi03@gmail.com>
2025-01-08 20:34:45 +0545

    Update metadata description and create robots.txt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { ToastContainer } from "react-toastify";
 export const metadata: Metadata = {
   title: "UnityWorks: Empowering Communities for Sustainable Growth",
   description:
-    "UnityWorks, a Pokhara-based cooperative, fosters sustainable growth through community-driven initiatives, offering financial support, skill development, and collaborative opportunities for local empowerment.",
+    "UnityWorks, a Pokhara-based cooperative, drives sustainable growth through financial support, skill development, and community empowerment initiatives..",
   keywords:
     "UnityWorks, unity, works, unity works, cooperative, Pokhara, Nepal, sustainable growth, community empowerment, financial support, skill development, local initiatives, collaborative opportunities, community-driven projects, social enterprise, member-owned cooperative, economic development, community programs, financial services",
 };

5905fa77402b3c861f29ed19bbbe0be22befcce2
Sajag Subedi <sajagsubedi03@gmail.com>
2025-01-12 18:38:38 +0545

    feat: add delete notice route in api

--- src/app/api/notices/[id]/route.ts
+++ src/app/api/notices/[id]/route.ts
@@ -4,6 +4,7 @@ import { NextResponse } from "next/server";
 import mongoose from "mongoose";
 import { checkAuth } from "@/middlewares/checkAuth.middleware";
 import { dbQueryType, Visibility } from "@/types/ApiTypes";
+import cloudinary from "@/lib/cloudinary";
 
 // Route 1: to fetch notice by ID
 export const GET = async (
@@ -26,7 +27,7 @@ export const GET = async (
 
     const auth = await checkAuth(); //checking auth
     if (!auth || !auth.isAuthenticated) {
-      dbQuery["visibility"] = Visibility.PUBLIC; // Fetch public news only if not authenticated
+      dbQuery["visibility"] = Visibility.PUBLIC; // Fetch public notice only if not authenticated
     }
     const notice = await Notice.findOne(dbQuery);
 
@@ -39,7 +40,65 @@ export const GET = async (
 
     return NextResponse.json({ success: true, notice }, { status: 200 });
   } catch (err) {
-    console.error("Error fetching news:", err);
+    console.error("Error fetching notice:", err);
+    return NextResponse.json(
+      { success: false, message: "Something went wrong. Please try again." },
+      { status: 500 }
+    );
+  }
+};
+
+//Route 2:to update notice
+export const PATCH = async (request: Request) => {
+  return NextResponse.json(
+    { success: true, message: "Notice updated successfully!" },
+    { status: 200 }
+  );
+};
+
+// Route 3 : to delete notice by ID
+export const DELETE = async (
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) => {
+  await connectDb();
+  try {
+    const auth = await checkAuth(); //checking auth
+    if (!auth || !auth.isAuthenticated) {
+      return NextResponse.json(
+        { success: false, message: "Unauthorized" },
+        { status: 401 }
+      );
+    }
+    const { id } = await params;
+
+    if (mongoose.Types.ObjectId.isValid(id) === false) {
+      return NextResponse.json(
+        { success: false, message: "Invalid notice ID" },
+        { status: 400 }
+      );
+    }
+
+    const notice = await Notice.findOneAndDelete({ _id: id });
+
+    if (!notice) {
+      return NextResponse.json(
+        { success: false, message: "Notice not found" },
+        { status: 404 }
+      );
+    }
+
+    //delete image from cloudinary
+    notice.images.map(async (image) => {
+      await cloudinary.uploader.destroy(image.public_id);
+    });
+
+    return NextResponse.json(
+      { success: true, message: "Notice deleted successfully!" },
+      { status: 200 }
+    );
+  } catch (err) {
+    console.error("Error deleting notice:", err);
     return NextResponse.json(
       { success: false, message: "Something went wrong. Please try again." },
       { status: 500 }

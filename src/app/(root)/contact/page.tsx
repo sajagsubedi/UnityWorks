fd0fe8d1f143adb516f0a145ba041f2294084e2d
Sajag Subedi <sajagsubedi03@gmail.com>
2025-01-19 19:46:24 +0545

    Refactor: removed react hook form in contact

--- src/app/(root)/contact/page.tsx
+++ src/app/(root)/contact/page.tsx
@@ -1,33 +1,40 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
-import { useForm } from "react-hook-form";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { toast } from "react-toastify";
 import ContactBanner from "@/assets/contact-banner.jpg";
-
-interface FormDataType {
-  email: string;
-  name: string;
-  subject: string;
-  message: string;
-}
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 export default function ContactPage() {
-  const { register, handleSubmit, reset } = useForm();
+  const defaultValue = {
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  };
 
-  const onSubmit = async (data: FormDataType) => {
+  const [formData, setFormData] = useState(defaultValue);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    setLoading(true);
+    e.preventDefault();
     try {
-      const response = await axios.post("/api/contact", data, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
+      const response = await axios.post("/api/contact", formData);
       toast.success(response.data.message);
-      reset(); // Reset form fields after successful submission
-    } catch (error) {
-      toast.error("Failed to send message");
+      setFormData(defaultValue);
+      setLoading(false);
+    } catch (err) {
+      const error = err as AxiosError;
+      toast.error(error.message);
     }
   };
 
@@ -58,7 +65,7 @@ export default function ContactPage() {
         <h2 className="text-center font-bold text-gray-600">Message Us</h2>
         <form
           className="max-w-[480px] sm:w-full w-auto mx-auto flex flex-col"
-          onSubmit={handleSubmit(onSubmit)}
+          onSubmit={handleSubmit}
         >
           <div>
             <label htmlFor="name" className="leading-7 text-sm text-gray-600">
@@ -67,7 +74,9 @@ export default function ContactPage() {
             <input
               type="text"
               id="name"
-              {...register("name", { required: true })}
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
               className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
             />
           </div>
@@ -78,7 +87,9 @@ export default function ContactPage() {
             <input
               type="email"
               id="email"
-              {...register("email", { required: true })}
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
               className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
             />
           </div>
@@ -92,7 +103,9 @@ export default function ContactPage() {
             <input
               type="text"
               id="subject"
-              {...register("subject", { required: true })}
+              name="subject"
+              value={formData.subject}
+              onChange={handleChange}
               className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
             />
           </div>
@@ -105,14 +118,22 @@ export default function ContactPage() {
             </label>
             <textarea
               id="message"
-              {...register("message", { required: true })}
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
               className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
             ></textarea>
           </div>
           <button
-            className="w-full px-5 py-3 mt-5 bg-green-500 rounded text-white"
+            className="w-full px-5 py-3 mt-5 bg-green-500 rounded text-white flex justify-center items-center gap-2 disabled:bg-green-400"
             type="submit"
+            disabled={loading}
           >
+            <AiOutlineLoading3Quarters
+              className={`animate-spin text-xl ${
+                loading ? "opacity-100" : "opacity-0"
+              } `}
+            />
             Send Message
           </button>
         </form>
